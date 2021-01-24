31c232722053e4885aae67ddbb57989ffcc6577c
Lakshin Karunaratne <LakshinK@99x.lk>
2021-01-24 19:00:14 +0530

    Creating BankApp

---
+++ BankApp/BankApp.ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using BankApp.Service;
+
+namespace BankApp.ConsoleApp
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			var name = "Mr.Bryan Walton";
+			BankAccount ba = new BankAccount(name, 11.99m);
+			Console.WriteLine("Hello {0}. Your opening balance is ${1}", name, ba.Balance);
+
+			ba.Credit(5.77m);
+			Console.WriteLine("Credited: ${0}", 5.77);
+
+			ba.Debit(11.22m);
+			Console.WriteLine("Debited: ${0}", 11.22);
+
+			Console.WriteLine("Current balance is ${0}", ba.Balance);
+		}
+	}
+}
---
+++ BankApp/BankApp.Service/BankAccount.cs
@@ -0,0 +1,66 @@
+﻿/*https://docs.microsoft.com/en-us/visualstudio/test/walkthrough-creating-and-running-unit-tests-for-managed-code?view=vs-2019*/
+
+using System;
+
+namespace BankApp.Service
+{
+	/// <summary>
+	/// Bank account demo class.
+	/// </summary>
+	public class BankAccount
+	{
+		private readonly string _customerName;
+		private decimal _balance;
+
+		public BankAccount(string customerName, decimal balance)
+		{
+			_customerName = customerName;
+			_balance = balance;
+		}
+
+		public string CustomerName
+		{
+			get { return _customerName; }
+		}
+
+		public decimal Balance
+		{
+			get { return _balance; }
+		}
+
+		public void Debit(decimal amount)
+		{
+			if (amount > _balance)
+			{
+				throw new ArgumentOutOfRangeException("amount");
+			}
+
+			if (amount < 0)
+			{
+				throw new ArgumentOutOfRangeException("amount");
+			}
+
+			this.AddAmountToBalance(amount);
+		}
+
+		private void AddAmountToBalance(decimal amount)
+		{
+			_balance += amount; // intentionally incorrect code
+		}
+
+		public void Credit(decimal amount)
+		{
+			if (amount < 0)
+			{
+				throw new ArgumentOutOfRangeException("amount");
+			}
+
+			this.SubstractAmountFromBalance(amount);
+		}
+
+		private void SubstractAmountFromBalance(decimal amount)
+		{
+			_balance += amount;
+		}
+	}
+}
